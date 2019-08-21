1dedec94626375d5ce5a5f608226c9de6bb47e2b
Administrator <771968690@qq.com>
2019-08-21 09:29:42 +0800

    sunpeng

--- rose-server-content/src/main/java/com/rose/conf/Application.java
+++ rose-server-content/src/main/java/com/rose/conf/Application.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import tk.mybatis.spring.annotation.MapperScan;
 
@@ -45,10 +46,10 @@ public class Application extends WebMvcConfigurationSupport {
         registry.addInterceptor(loginInterceptor).addPathPatterns("/user/**");
     }
 
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
-//    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+    }
 
     @Bean
     StringRedisTemplate template(RedisConnectionFactory connectionFactory) {
