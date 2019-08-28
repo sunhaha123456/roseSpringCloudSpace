28b7b62e43016f1b035bb71f4934ee420ac134b4
Administrator <771968690@qq.com>
2019-08-28 21:50:49 +0800

    sunpeng 初步实现网关

--- rose-server-gateway/src/main/java/com/rose/conf/Application.java
+++ rose-server-gateway/src/main/java/com/rose/conf/Application.java
@@ -2,25 +2,9 @@ package com.rose.conf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import tk.mybatis.spring.annotation.MapperScan;
 
-@RestController
-@ComponentScan("com.rose")
-@MapperScan("com.rose.dbopt.mapper")
 @SpringBootApplication
-public class Application extends WebMvcConfigurationSupport {
-
-    @RequestMapping("/home")
-    String home() {
-        return "Hello World!";
-    }
+public class Application {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
@@ -31,8 +15,8 @@ public class Application extends WebMvcConfigurationSupport {
 //        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
 //    }
 
-    @Bean
-    StringRedisTemplate template(RedisConnectionFactory connectionFactory) {
-        return new StringRedisTemplate(connectionFactory);
-    }
+//    @Bean
+//    StringRedisTemplate template(RedisConnectionFactory connectionFactory) {
+//        return new StringRedisTemplate(connectionFactory);
+//    }
 }
